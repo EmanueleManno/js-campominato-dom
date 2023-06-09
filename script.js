3644ebb50a622749dc7971dd5a17b0c467542146
EmanueleManno <mannus1994@gmail.com>
2023-06-09 08:46:42 +0200

    Gestione delle celle contententi le bombe

--- script.js
+++ script.js
@@ -13,6 +13,42 @@ console.log(scorePlaceholder);
 
 //FUNZIONE CHE CONTIENE LA LOGICA DI GIOCO:
 const startGame = () => {
+    let isGameOver = false;
+
+      //FUNZIONE GENERA BOMBE:
+      const generateBombs = (numberOfBombs, maxNumber) => {
+        let bombs = [];
+
+        while(bombs.length < numberOfBombs) {
+            let randomNumber;
+            do {
+            randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+        }
+            while(bombs.includes(randomNumber));
+            bombs.push(randomNumber);
+        }
+
+        return bombs;
+    }
+
+    //FUNZIONE PER CREARE LE CELLE:
+    const createCell = (cellNumber, level) => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell', level);
+        cell.append(cellNumber);
+        return cell;
+}
+
+//FUNZIONE TERMINA PARTITA:
+function endGame(score, hasHitBomb){
+    const message = hasHitBomb 
+    ? `Fine! Hai raggiunto ${score} punti` 
+    : `Hai perso! Totale punti: + ${score}`;
+
+    alert(message);
+
+    isGameOver = true;
+}
 
     //SE VOGLIO FARE UNA NUOVA PARTITA:
     button.innerText ='Nuova Partita';
@@ -61,30 +97,6 @@ const startGame = () => {
     const bombs = generateBombs(totalBombs, totalCells);
     console.log(bombs);
 
-    //FUNZIONE GENERA BOMBE:
-    function generateBombs(numberOfBombs, maxNumber) {
-        let bombs = [];
-
-        while(bombs.length < numberOfBombs) {
-            let randomNumber;
-            do {
-            randomNumber = Math.floor(Math.random() * maxNumber) + 1;
-        }
-            while(bombs.includes(randomNumber));
-            bombs.push(randomNumber);
-        }
-
-        return bombs;
-    }
-
-    //FUNZIONE PER CREARE LE CELLE:
-    const createCell = (cellNumber, level) => {
-        const cell = document.createElement('div');
-        cell.classList.add('cell', level);
-        cell.append(cellNumber);
-        return cell;
-}
-
     for(let i = 1; i <= totalCells; i++) {
 
         //CREO LA CELLA:
@@ -94,14 +106,26 @@ const startGame = () => {
         cell.addEventListener('click', () => {
 
             //CONTROLLO SE ERA STATA GIA' CLICCATA:
-            if(cell.classList.contains('clicked')) return;
-
-            //INCREMENTO IL PUNTEGGIO:
-            scorePlaceholder.innerText = ++score;
+            if(isGameOver === true || cell.classList.contains('clicked')) return;
 
             //AGGIUNGO LA CLASSE CLICKED:
             cell.classList.add('clicked');
             console.log(cell.innerText);
+
+            //CONTROLLO SE E' BUONA O BOMBA:
+            const hasHitBomb = bombs.includes(i);
+
+            if(hasHitBomb){
+                cell.classList.add('bomb');
+                endGame(score, hasHitBomb);
+            } else {
+                 //INCREMENTO IL PUNTEGGIO:
+                 scorePlaceholder.innerText = ++score;
+            if(score === maxPoints){
+                endGame(score, hasHitBomb);
+            }
+            }
+
         })
 
         //LA INSERISCO IN PAGINA:
