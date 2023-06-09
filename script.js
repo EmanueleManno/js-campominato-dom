d90efc0f4bc3ed1f3692e2f42b68b6927456884b
EmanueleManno <mannus1994@gmail.com>
2023-06-09 03:11:13 +0200

    Implementazione del livello di difficoltà

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const grid = document.getElementById('grid');
 console.log(grid);
 const button = document.querySelector('button');
 console.log(button);
+const levelSelect = document.getElementById('bottone-difficolta');
+console.log(levelSelect);
 
 //FUNZIONE CHE CONTIENE LA LOGICA DI GIOCO:
 const startGame = () => {
@@ -16,21 +18,49 @@ const startGame = () => {
     //SVUOTO LA PAGINA:
     grid.innerText = '';
 
+    //RECUPERO IL LIVELLO:
+    const level = levelSelect.value;
+
+    //METTO LA CLASSE LIVELLO:
+    grid.classList.add(level);
+
+    //CELLE TOTALI:
+    let rows;
+    let cols;
+
+    switch(level) {
+        case 'difficile':
+            rows = 7;
+            cols = 7;
+            break;
+        case 'intermedio':
+            rows = 9;
+            cols = 9;
+            break;
+        case 'facile':
+            rows = 10;
+            cols = 10;
+            break;
+    }
+
+    //CALCOLO LE CELLE TOTALI:
+    const totalCells = rows * cols;
+
     //FUNZIONE PER CREARE LE CELLE:
-    const createCell = (cellNumber) => {
+    const createCell = (cellNumber, level) => {
         const cell = document.createElement('div');
-        cell.classList.add('cell');
+        cell.classList.add('cell', level);
         cell.append(cellNumber);
         return cell;
 }
 
-    for(let i = 1; i <= 100; i++) {
+    for(let i = 1; i <= totalCells; i++) {
 
         //CREO LA CELLA:
         const cell = createCell(i);
 
         //AGGANCIARE L'EVENT LISTENER:
-        cell.addEventListener('click',() => {
+        cell.addEventListener('click', () => {
             cell.classList.add('clicked');
             console.log(cell.innerText);
         })
